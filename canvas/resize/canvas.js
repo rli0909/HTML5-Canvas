202eaed0f5e011bf76a367701a8549a6aad0c652
Lucy <rli12@wou.edu>
2019-09-04 10:55:37 -0700

    added resized canvas

---
+++ canvas/resize/canvas.js
@@ -0,0 +1,13 @@
+// console.log('r/place');
+
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var context =  canvas.getContext('2d');
+
+context.fillRect(100, 100, 100, 100);
+
+console.log(canvas);
+
