11cc511a2a096598457c14e511d3e388bec25d5a
Lucy <rli12@wou.edu>
2019-09-04 16:22:49 -0700

    added lines

--- canvas/resize/canvas.js
+++ canvas/resize/canvas.js
@@ -7,7 +7,21 @@ canvas.height = window.innerHeight;
 
 var context =  canvas.getContext('2d');
 
+context.fillStyle = "#FF3855";
 context.fillRect(100, 100, 100, 100);
+context.fillStyle = "#FFF700";
+context.fillRect(400, 100, 100, 100);
+context.fillStyle = "#50BFE6";
+context.fillRect(700, 100, 100, 100);
 
 console.log(canvas);
+
+// Line
+context.beginPath();
+context.moveTo(200, 200);
+context.lineTo(300, 300);
+context.lineTo(400, 200);
+context.strokeStyle = "#FF5470";
+context.stroke();
+
 
